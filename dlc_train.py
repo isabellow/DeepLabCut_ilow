629821d5def39c875ab813d5c7fc26ee56dee2f8
isabellow <ilow@stanford.edu>
2019-03-18 15:09:21 -0700

    added create_training_dataset to update to linux paths

--- dlc_train.py
+++ dlc_train.py
@@ -2,8 +2,13 @@ import sys
 import deeplabcut
 
 """ train deeplabcut on Sherlock (~6hrs on GPU for 200,000 iterations)
+also creates training set again to ensure paths are updated for linux
+
 NOTE: should already have created a DLC project and labeled training set
 	see: DeepLabCut/examples/Demo_yourowndata.ipynb
+
+NOTE: be sure to update paths in config and pose files
+	(if project was created elsewhere)
 
 Params:
 -------
@@ -17,4 +22,5 @@ if not os.path.exists(path_config_file):
     raise ValueError('no config file at %s' %path_config_file)
 
 # train the network
+deeplabcut.create_training_dataset(path_config_file, windows2linux=True)
 deeplabcut.train_network(path_config_file, gputouse=1, max_snapshots_to_keep=5)
